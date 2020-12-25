ff71c5fb2b562c5fc4a8944ffd975c7cfd305ded
portal-S <aligus321@mail.ru>
2020-12-25 11:22:12 +0300

    “day2”

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,15 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        if(i >= 1 && i <= 4) System.out.println("Малоэтажный дом");
+        else if(i >= 5 && i <= 8) System.out.println("Среднеэтажный дом");
+        else if(i >= 9) System.out.println("Многоэтажный дом");
+        else System.out.println("Ошибка ввода");
+        scanner.close();
     }
 }
--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,19 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        if(a < b){
+            a++;
+            while (a < b){
+                if(a % 5 == 0 && a % 10 != 0) System.out.print(a + " ");
+                a++;
+            }
+        } else System.out.println("Некорректный ввод");
+        scanner.close();
     }
 }
