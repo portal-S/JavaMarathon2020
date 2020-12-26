6ae8ba59b6ebd07723fc898f0f21bfe88092ebd3
portal-S <aligus321@mail.ru>
2020-12-26 23:29:18 +0300

    day3

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,19 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()){
+            double d1 = scanner.nextDouble();
+            double d2 = scanner.nextDouble();
+            if(d2 != 0){
+                System.out.println(d1 / d2);
+                continue;
+            }
+            break;
+        }
 
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,18 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 5; i++){
+            double d1 = scanner.nextDouble();
+            double d2 = scanner.nextDouble();
+            if (d2 == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(d1 / d2);
+        }
     }
 }
