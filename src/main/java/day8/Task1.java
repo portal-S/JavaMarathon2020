59ae01d7b7f2add7965975aca1def118e6d4f3d4
portal-S <aligus321@mail.ru>
2021-01-03 14:44:56 +0300

    day8

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,10 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        StringBuilder s = new StringBuilder("0");
+        for (int i = 1; i <= 20000; i++){
+            s.append(" " + i);
+        }
+        System.out.println(s);
     }
 }
