e1cd413e474cca51d11c2369b989804dab94a746
portal-S <aligus321@mail.ru>
2021-01-09 22:10:06 +0300

    day15

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,24 @@
 package day15;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
+public class Task1 {
+    public static void main(String[] args) throws IOException {
+        File file = new File("src/main/resources/missing_shoes.txt");
+        File file1 = new File("src/main/resources/shoes.csv");
+        if(!file.exists()) file.createNewFile();
+        Scanner scanner = new Scanner(file1);
+        PrintWriter printWriter = new PrintWriter(file);
+        while (scanner.hasNext()){
+            String s = scanner.nextLine();
+            String[] info = s.split(";");
+            if(Integer.parseInt(info[2]) == 0) printWriter.println(s);
+        }
+        scanner.close();
+        printWriter.close();
     }
 }
